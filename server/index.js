79889fcc117f4028d37901f6795d5ee84da4b68a
jeikai <98737616+jeikai@users.noreply.github.com>
2024-02-21 20:34:19 +0700

    init server

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+const routers = require("./routes")
