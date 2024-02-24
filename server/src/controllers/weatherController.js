4ed95d43e611fb059441d562b824b3b72bd576a3
anhson <sonanhnguyen003@gmail.com>
2024-02-24 22:56:59 +0700

    feat: plants disease identification

--- server/src/controllers/weatherController.js
+++ server/src/controllers/weatherController.js
@@ -4,31 +4,7 @@ import {
 	OPEN_WEATHER_URL,
 	GGMAP_AQI_URL,
 } from '../config/const.js';
-import { AIR_VISUAL_AQI_URL } from '@/config/const.js';
-/**
- * Retrieves the latitude and longitude of a given location.
- * @param {string} location - The address or location to retrieve the coordinates for.
- * @returns {Promise<{ lat: number, lng: number } | null>} The latitude and longitude of the location, or null if an error occurred.
- */
-const getLatAndLng = async (location) => {
-	try {
-		const geoInfo = await axios.get(GGMAP_GEOCODE_URL, {
-			params: {
-				address: location,
-				key: process.env.GGMAP_API_KEY,
-			},
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-		const { lat, lng } = geoInfo.data.results[0].geometry.location;
-		return { lat, lng };
-	} catch (error) {
-		console.log('Error fetch lat and lng', error);
-		return null;
-	}
-};
-
+import { getLatAndLng } from '../utils/utils.js';
 const getCurrentWeatherData = async (req, res) => {
 	const { location } = req.query;
 	try {
---
+++ server/src/model/Plant.js
@@ -0,0 +1,55 @@
+import { url } from 'inspector';
+import mongoose, { Schema } from 'mongoose';
+
+const plantSchema = new Schema({
+	plant_id: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	common_names: {
+		type: String,
+		required: true,
+	},
+	url: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+
+	edible_parts: {
+		type: String,
+		required: false,
+	},
+	watering: {
+		min: Number,
+		max: Number,
+	},
+});
+
+const Plant = new mongoose.model('Plant', plantSchema);
+
+const addNewPlant = async (plantData) => {
+	const newPlant = new Plant(plantData);
+	await newPlant.save();
+	return newPlant;
+};
+const deletePlant = async (plantId) => {
+	const plant = await Plant.findByIdAndDelete(plantId);
+	return plant;
+};
+const getPlant = async (plantId) => {
+	const plant = await Plant.findById(plantId);
+	return plant;
+};
+export default {
+	addNewPlant,
+	deletePlant,
+	getPlant,
+};
